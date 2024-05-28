cfebc45baacc91b9abc3c32c8d8536cfa20458b0
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-05-28 23:06:18 +0600

    change thankyou

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,18 +1,19 @@
+import React, { useEffect } from "react";
+
 export default function ThankYou() {
-  setTimeout(function () {
-    window.location.href = "https://baiyr-emilov-nike.netlify.app/";
-  }, 5000)
-  window.onload = function() {
-    window.scrollTo(0, 0);
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "https://adorniya.netlify.app/"; // Redirect URL after 5 seconds
+    }, 5000);
+    window.scrollTo(0, 0); // Scroll to the top of the page on component load
+  }, []);
+
   return (
-    <div className="ThankYou" style={{
-      textAlign: "center",
-    }}>
-      <h1>Thank you!</h1>
-      <p>
-        Your order has been received. Our specialists will contact with you
-        shortly to confirm it.
+    <div className="thank-you">
+      <h1 className="thank-you-heading">Thank you for your order!</h1>
+      <p className="thank-you-message">
+        Your order has been received. Our specialists will contact you shortly
+        to confirm it.
       </p>
     </div>
   );
