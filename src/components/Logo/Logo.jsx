cca604764c5c9c4cca887ee09df502167c309d91
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-05-28 23:04:48 +0600

    added new logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import "./Logo.css";
-import logo from "../../assets/nike-logo.png";
+import logo from "../../assets/2-removebg-preview.png";
 import { NavLink } from "react-router-dom";
 
 export default function Logo() {
