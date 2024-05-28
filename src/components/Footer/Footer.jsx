6d53df26a9926fef1824595c3331d189361f0b3e
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-05-28 23:04:32 +0600

    change constructor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer">
       <div className="container">
-        <div className="row">
-          <div className="col-md-3">
+        <div className="footer-row">
+          <div className="footer-col">
             <h3>Site Map</h3>
             <ul>
               <li>
@@ -18,56 +18,55 @@ const Footer = () => {
                 <Link to="/about">About Us</Link>
               </li>
               <li>
-                <Link to="/deliver">Deliver</Link>
+                <Link to="/delivery">Delivery</Link>
               </li>
               <li>
                 <Link to="/contact">Contact Us</Link>
               </li>
             </ul>
           </div>
-          <div className="col-md-3">
+          <div className="footer-col">
             <h3>Our Services</h3>
             <ul>
               <li>
-                <Link to={"/categories/tracksuits"}>Tracksuits</Link>
+                <Link to="/categories/piercing">Piercing</Link>
               </li>
               <li>
-                <Link to={"/categories/hats"}>Different Hats</Link>
+                <Link to="/categories/chains">Chains</Link>
               </li>
               <li>
-                <Link to={"/categories/backpack"}>Backpack</Link>
+                <Link to="/categories/ring">Ring</Link>
               </li>
               <li>
-                <Link to={"/categories/sneakers"}>Sneakers</Link>
+                <Link to="/categories/bracelets">Bracelets</Link>
+              </li>
+              <li>
+                <Link to="/categories/pendants">Pendants</Link>
+              </li>
+              <li>
+                <Link to="/categories/earrings">Earrings</Link>
+              </li>
+              <li>
+                <Link to="/categories/keyrings">Keyrings</Link>
               </li>
             </ul>
           </div>
-          <div className="col-md-3">
+          <div className="footer-col">
             <h3>Contact Us</h3>
             <p>
-              1 Esenina
-              <br /> Karakol, Kyrgyzstan
+              1 Esenina<br />
+              Karakol, Kyrgyzstan
             </p>
             <p>
-            <a
-            className="Tel"
-            target="_blank"
-            rel="noreferrer"
-            href="tel:  +996 707 763 896"
-          >
-            +996 707 763 896
-          </a>
-              <br /> Email:    <a
-            className="Tel"
-            target="_blank"
-            rel="noreferrer"
-            href="mailto:emilovbaku530@gmail.com"
-          >
-            emilovbaku530@gmail.com
-          </a>
+              <a className="contact-link" href="tel:+996704465375">
+                +996 704 465 375
+              </a><br />
+              Email: <a className="contact-link" href="mailto:gulanda727@gmail.com">
+                gulanda727@gmail.com
+              </a>
             </p>
           </div>
-          <div className="col-md-3">
+          <div className="footer-col">
             <h3>Follow Us</h3>
             <ul className="social-media">
               <li>
